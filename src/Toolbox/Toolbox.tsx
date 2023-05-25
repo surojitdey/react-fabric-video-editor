1b1627dc9ce3563f818f582a25e07a0a2590a82e
Ziyue (Bella) Chen <77322443+zic115@users.noreply.github.com>
2023-05-24 22:16:27 -0700

    Update Toolbox.tsx

--- src/Toolbox/Toolbox.tsx
+++ src/Toolbox/Toolbox.tsx
@@ -3,6 +3,10 @@ import {
   Box, Button, Divider, Grid, IconButton, Stack, Typography,
 } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
+import AutoAwesomeMosaicTwoToneIcon from '@mui/icons-material/AutoAwesomeMosaicTwoTone';
+import TuneTwoToneIcon from '@mui/icons-material/TuneTwoTone';
+import InterestsTwoToneIcon from '@mui/icons-material/InterestsTwoTone';
+import VideocamTwoToneIcon from '@mui/icons-material/VideocamTwoTone';
 import TextFieldsTwoTone from '@mui/icons-material/TextFieldsTwoTone';
 import TextSnippetTwoToneIcon from '@mui/icons-material/TextSnippetTwoTone';
 import ZoomInTwoToneIcon from '@mui/icons-material/ZoomInTwoTone';
@@ -10,6 +14,8 @@ import ZoomOutTwoToneIcon from '@mui/icons-material/ZoomOutTwoTone';
 import BrushTwoToneIcon from '@mui/icons-material/BrushTwoTone';
 import NearMeTwoToneIcon from '@mui/icons-material/NearMeTwoTone';
 import ImageSearchTwoToneIcon from '@mui/icons-material/ImageSearchTwoTone';
+import AutoGraphTwoToneIcon from '@mui/icons-material/AutoGraphTwoTone';
+import PhotoFilterTwoToneIcon from '@mui/icons-material/PhotoFilterTwoTone';
 import Modal from '@mui/material/Modal';
 import { RootState } from '../redux/store';
 import ObjectAnimation from './objectAnimation';
@@ -64,80 +70,166 @@ export default function Toolbox() {
     >
       <Box display="flex" flexDirection="row" justifyContent="space-between" height="100%">
         <Grid
-          width="100px"
+          width="150px"
+          height="90%"
           sx={{
             position: 'fixed',
-            left: (theme) => theme.spacing(2),
-            top: (theme) => theme.spacing(2),
-            boxShadow: (theme) => theme.shadows[1],
+            top: (theme) => theme.spacing(8),
+            boxShadow: (theme) => theme.shadows[2],
           }}
           container
         >
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <IconButton
-              color="primary"
+              sx={{ color: 'black' }}
               onClick={() => {
                 canvas.isDrawingMode = false;
               }}
             >
               <NearMeTwoToneIcon width="100%" />
+              <div style={{ fontSize: '16px' }}>Select</div>
             </IconButton>
           </Grid>
-          <Grid item xs={6}>
-            <IconButton
-              onClick={() => {
-                dispatch(addText());
-              }}
-              color="primary"
-            >
-              <TextFieldsTwoTone width="100%" />
-            </IconButton>
-          </Grid>
-          <Grid item xs={6}>
-            <ImageUploadButton />
-          </Grid>
-          <Grid item xs={6}>
-            <IconButton color="primary" onClick={handleOpen}>
-              <ImageSearchTwoToneIcon width="100%" />
 
-            </IconButton>
-          </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <IconButton
+              sx={{ color: 'black' }}
               onClick={() => {
                 canvas.isDrawingMode = true;
               }}
-              color="primary"
             >
               <BrushTwoToneIcon width="100%" />
+              <div style={{ fontSize: '16px' }}>Draw</div>
             </IconButton>
           </Grid>
-          <Grid item xs={6}>
+
+          <Grid item xs={12}>
             <IconButton
-              disabled
+              sx={{ color: 'black' }}
+              onClick={() => {
+                canvas.isDrawingMode = false;
+              }}
+            >
+              <AutoAwesomeMosaicTwoToneIcon width="100%" />
+              <div style={{ fontSize: '16px' }}>Template</div>
+            </IconButton>
+          </Grid>
+
+          <Grid item xs={12}>
+            <IconButton
+              sx={{ color: 'black' }}
               onClick={() => {
                 dispatch(addText());
               }}
-              color="primary"
+            >
+              <TuneTwoToneIcon width="100%" />
+              <div style={{ fontSize: '16px' }}>Customize</div>
+            </IconButton>
+          </Grid>
+
+          <Grid item xs={12}>
+            <IconButton
+              sx={{ color: 'black' }}
+              onClick={() => {
+                dispatch(addText());
+              }}
+            >
+              <InterestsTwoToneIcon width="100%" />
+              <div style={{ fontSize: '16px' }}>Elements</div>
+            </IconButton>
+          </Grid>
+
+          <Grid item xs={12}>
+            <IconButton
+              sx={{ color: 'black' }}
+              onClick={handleOpen}
+            >
+              <ImageSearchTwoToneIcon width="100%" />
+              <div style={{ fontSize: '16px' }}>Images</div>
+            </IconButton>
+          </Grid>
+
+          <Grid item xs={12}>
+            <IconButton
+              sx={{ color: 'black' }}
+              onClick={handleOpen}
+            >
+              <VideocamTwoToneIcon width="100%" />
+              <div style={{ fontSize: '16px' }}>Videos</div>
+            </IconButton>
+          </Grid>
+
+          <Grid item xs={12}>
+            <ImageUploadButton />
+          </Grid>
+
+          <Grid item xs={12}>
+            <IconButton
+              sx={{ color: 'black' }}
+              onClick={() => {
+                dispatch(addText());
+              }}
+            >
+              <TextFieldsTwoTone width="100%" />
+              <div style={{ fontSize: '16px' }}>Text</div>
+            </IconButton>
+          </Grid>
+
+          <Grid item xs={12}>
+            <IconButton
+              sx={{ color: 'black' }}
+              onClick={() => {
+                dispatch(addText());
+              }}
+            >
+              <AutoGraphTwoToneIcon width="100%" />
+              <div style={{ fontSize: '16px' }}>Graphics</div>
+            </IconButton>
+          </Grid>
+
+          <Grid item xs={12}>
+            <IconButton
+              sx={{ color: 'black' }}
+              onClick={() => {
+                dispatch(addText());
+              }}
+            >
+              <PhotoFilterTwoToneIcon width="100%" />
+              <div style={{ fontSize: '16px' }}>Pixabay</div>
+            </IconButton>
+          </Grid>
+
+          <Grid item xs={12}>
+            <IconButton
+              disabled
+              sx={{ color: 'black' }}
+              onClick={() => {
+                dispatch(addText());
+              }}
             >
               <ZoomInTwoToneIcon width="100%" />
+              <div style={{ fontSize: '16px' }}>Zoom In</div>
             </IconButton>
           </Grid>
-          <Grid item xs={6}>
+
+          <Grid item xs={12}>
             <IconButton
               disabled
+              sx={{ color: 'black' }}
               onClick={() => {
                 dispatch(addText());
               }}
-              color="primary"
             >
               <ZoomOutTwoToneIcon width="100%" />
+              <div style={{ fontSize: '16px' }}>Zoom Out</div>
             </IconButton>
           </Grid>
+
           <Grid item xs={12}>
             <ObjectAnimation />
           </Grid>
         </Grid>
+
         <Stack
           sx={{
             backgroundColor: '#fff',
@@ -167,34 +259,48 @@ export default function Toolbox() {
 
       </Box>
       <Button
-        onClick={download}
+        onClick={() => dispatch(reset())}
         sx={{
+          width: 130,
           position: 'fixed',
           zIndex: 9999,
-          top: (theme) => theme.spacing(2),
-          right: (theme) => theme.spacing(2),
+          top: (theme) => theme.spacing(1.7),
+          right: (theme) => theme.spacing(38),
+        }}
+        color="error"
+        variant="contained"
+      >
+        Remove ALL
+      </Button>
+      <Button
+        onClick={download}
+        sx={{
+          width: 130,
+          position: 'fixed',
+          zIndex: 9999,
+          top: (theme) => theme.spacing(1.7),
+          right: (theme) => theme.spacing(20),
         }}
         color="success"
-        variant="outlined"
+        variant="contained"
       >
         Download
-
       </Button>
       <Button
         onClick={() => dispatch(reset())}
         sx={{
+          color: 'black',
+          background: 'white',
+          width: 130,
           position: 'fixed',
           zIndex: 9999,
-          top: (theme) => theme.spacing(2),
-          right: '140px',
+          top: (theme) => theme.spacing(1.7),
+          right: (theme) => theme.spacing(2),
         }}
-        color="error"
-        variant="outlined"
+        variant="contained"
       >
-        Remove ALL
-
+        Logout
       </Button>
-
       <Modal
         open={open}
         onClose={handleClose}
